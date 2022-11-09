7a9f7421d42fb5c218d1b672adb7599c6dbfe591
Spongzi <lxlandsx@outlook.com>
2022-11-09 18:37:38 +0800

    解决文件跨域问题

---
+++ src/main/java/com/spongzi/config/WebConfig.java
@@ -0,0 +1,37 @@
+package com.spongzi.config;
+
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * 网络配置
+ *
+ * @author spongzi
+ * @date 2022/11/09
+ */
+@Configuration
+public class WebConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration config = new CorsConfiguration();
+
+        // 允许跨域的头部信息
+        config.addAllowedHeader("*");
+        // 允许跨域的方法
+        config.addAllowedMethod("*");
+        // 可访问的外部域
+        config.addAllowedOrigin("*");
+        // 需要跨域用户凭证（cookie、HTTP认证及客户端SSL证明等）
+        // config.setAllowCredentials(true);
+        // config.addAllowedOriginPattern("*");
+
+        // 跨域路径配置
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
