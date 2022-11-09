add5bc5dc001676964b8037a3761869ba777ba2b
Spongzi <lxlandsx@outlook.com>
2022-11-09 20:02:53 +0800

    实现逻辑删除功能

--- src/main/java/com/spongzi/domain/User.java
+++ src/main/java/com/spongzi/domain/User.java
@@ -1,9 +1,6 @@
 package com.spongzi.domain;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -97,6 +94,7 @@ public class User implements Serializable {
     /**
      * 是否删除(0-未删, 1-已删)
      */
+    @TableLogic
     private Integer isDeleted;
 
     @TableField(exist = false)
